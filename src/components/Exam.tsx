c3581ba821fe01754c3c0f1b0361f2f82caaf78a
ShotaTevdorasvhili <stevdorashvili08@gmail.com>
2025-09-09 09:41:07 +0400

    .

--- src/components/Exam.tsx
+++ src/components/Exam.tsx
@@ -1,8 +1,15 @@
 export function Exam() {
   return (
     <>
-      <h1>Exam</h1>
-      <button>Start Exam</button>
+      <h1>Rules</h1>
+      <p>
+        You'll be asked 30 multiple choise questions, after selecting all
+        answers and submitting the exam you would have to get at least 27
+        questions right for you to pass, every result is stored and used for
+        statistics in the leaderboards.
+      </p>
+
+      <h2>Good luck!</h2>
     </>
   );
 }
---
+++ src/services/examService.ts
@@ -0,0 +1,18 @@
+const api = import.meta.env.VITE_BACKEND_API;
+
+export async function getQuestions(category: number, language: string) {
+  const res = await fetch(
+    `${api}/questions/exam/${category}?language=${language}`,
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    }
+  );
+  if (!res.ok) {
+    const msg = await res.text();
+    throw new Error(msg || "failed to fetch questions");
+  }
+
+  return JSON.stringify(res);
+}
