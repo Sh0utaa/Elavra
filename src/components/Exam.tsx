4de8e2030f24672e275216ecfc4dbc77df826eb6
ShotaTevdorasvhili <stevdorashvili08@gmail.com>
2025-09-30 12:50:28 +0200

    exam works

--- src/components/Exam.tsx
+++ src/components/Exam.tsx
@@ -1,7 +1,119 @@
+import { useEffect, useReducer, useState } from "react";
+import { examReducer, initialExamState } from "../context/examReducer";
+import { getQuestions } from "../services/examService";
+import { submitAnswers as submitAnswersService } from "../services/examService";
+
 export function Exam() {
+  const [exam, dispatch] = useReducer(examReducer, initialExamState);
+  const [result, setResult] = useState({
+    incorrectAmount: 0,
+    failed: false,
+    correctAmount: 0,
+  });
+
+  useEffect(() => {}, [exam.answers]);
+
+  useEffect(() => {
+    async function fetchQeustions() {
+      try {
+        var questions = await getQuestions(1, "ka");
+        dispatch({
+          type: "EXAM_START",
+          payload: questions,
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchQeustions();
+  }, []);
+
+  async function submitAnswers() {
+    if (Object.keys(exam.answers).length < 30) {
+      alert("Must answer all 30 questions");
+      return;
+    }
+
+    try {
+      const res = await submitAnswersService(exam);
+      setResult({
+        correctAmount: res.correctAmount,
+        failed: res.failed,
+        incorrectAmount: res.incorrectAmount,
+      });
+
+      dispatch({
+        type: "EXAM_SUBMIT",
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <>
-      <h1>EXAM</h1>
+      {exam.started ? (
+        exam.submitted ? (
+          <div>
+            <h1>Exam Submitted</h1>
+            <ul>
+              {result.failed ? <li>you failed</li> : <li>you passed</li>}
+              <li>incorrectAmount: {result.incorrectAmount}</li>
+              <li>correctAmount: {result.correctAmount}</li>
+            </ul>
+          </div>
+        ) : (
+          <>
+            <h1>EXAM</h1>
+            <div>
+              <h2>{exam.questions[exam.currentIndex].questionContent}</h2>
+              <ul>
+                <p>question {exam.currentIndex + 1}</p>
+                {exam.questions[exam.currentIndex].answers.map((a) => (
+                  <li key={a.id}>
+                    <button
+                      onClick={() => {
+                        dispatch({
+                          type: "EXAM_ANSWER",
+                          payload: {
+                            questionId: exam.questions[exam.currentIndex].id,
+                            answerId: a.id,
+                          },
+                        });
+                      }}
+                      style={{
+                        backgroundColor:
+                          a.id ===
+                          exam.answers[exam.questions[exam.currentIndex].id]
+                            ? "#4CAF50"
+                            : "#f0f0f0",
+                      }}
+                    >
+                      {a.text}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <button
+              onClick={() => dispatch({ type: "EXAM_PREV" })}
+              disabled={exam.currentIndex === 0}
+            >
+              Prev
+            </button>
+            {exam.currentIndex + 1 === 30 ? (
+              <button onClick={submitAnswers}>Submit</button>
+            ) : (
+              <button onClick={() => dispatch({ type: "EXAM_NEXT" })}>
+                Next
+              </button>
+            )}
+          </>
+        )
+      ) : (
+        <p>Loading exam...</p>
+      )}
     </>
   );
 }
--- src/services/examService.ts
+++ src/services/examService.ts
@@ -1,3 +1,5 @@
+import type { ExamState } from "../context/examReducer";
+
 const api = import.meta.env.VITE_BACKEND_API;
 
 export async function getQuestions(category: number, language: string) {
@@ -12,6 +14,29 @@ export async function getQuestions(category: number, language: string) {
   if (!res.ok) {
     const msg = await res.text();
     throw new Error(msg || "failed to fetch questions");
+  }
+
+  return res.json();
+}
+
+export async function submitAnswers(state: ExamState) {
+  const requestBody = Object.entries(state.answers).map(
+    ([questionIdStr, answerId]) => ({
+      questionId: parseInt(questionIdStr),
+      answerId: answerId,
+    })
+  );
+
+  const res = await fetch(`${api}/questions/submit`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+    body: JSON.stringify(requestBody), // This will be the array format
+  });
+
+  if (!res.ok) {
+    const errorText = await res.text();
+    throw new Error(errorText || "Failed to submit answers");
   }
 
   return res.json();
